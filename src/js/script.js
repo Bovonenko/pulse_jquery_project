307a9b741586bca3acbd70d36f1deb43d13e8749
Rostyslav <bovonenko@icloud.com>
2022-05-06 13:19:35 +0200

    i am back again

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//       speed: 1200,
+//       // variableHeight: true,
+//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//       responsive: [
+//           {
+//             breakpoint: 992,
+//             settings: {
+//               arrows: false,
+//               dots: true
+//             }
+//           }
+//         ]
+//     });
+//   });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  navPosition: 'bottom',
+  // nav: false,
+  responsive: {
+    320: {
+      nav: true
+    },
+    992: {
+      nav: true
+    },
+    1200: {
+      nav: false
+    }
+  }
+});
+
+document.querySelector('.prev').addEventListener ('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener ('click', function () {
+  slider.goTo('next');
+});
+
+
+
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+   })
+  })
+}
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
+
